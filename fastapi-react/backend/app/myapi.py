86d2e63d47757b9dba60c7ef7066b9af6d77d41b
Hien Luong <hyhien@gmail.com>
2021-10-12 15:19:52 +0700

    CRUD Todo API

--- fastapi-react/backend/app/myapi.py
+++ fastapi-react/backend/app/myapi.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
+from pathlib import Path
+import os
+from .models.Todo import TodoItem
 
 
 app = FastAPI()
@@ -20,7 +24,112 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
+
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
+
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    folder = Path(__file__).parent
+    my_path_file = os.path.join(folder, "todo.json")
+    with open(my_path_file, "r") as the_file:
+        data = the_file.read()
+        print(data)
+
+    data = json.loads(data)
+    return { "data": data }
+
+folder = Path(__file__).parent
+my_path_file = os.path.join(folder, "todo.json")
+
+
+def read_todo_data():
+    with open(my_path_file, "r") as the_file:
+        data = the_file.read()
+
+    return json.loads(data)
+
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: TodoItem) -> dict:
+    data = read_todo_data()
+    data.append(
+        {
+            'id': todo.id,
+            'item': todo.item
+        }
+    )
+    print(data)
+
+    # Save
+    with open(my_path_file, "w") as the_file:
+        # the_file.write(json.dumps(data))
+        json.dump(data, the_file, indent=4)
+
+    return {
+        "data": { "Todo added." }
+    }
+
+
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: TodoItem) -> dict:
+    data = read_todo_data()
+    for todo in data:
+        if int(todo["id"]) == id:
+            todo["item"] = body.item
+
+            with open(my_path_file, "w") as the_file:
+                json.dump(data, the_file, indent=4)
+
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
+
+
+# API endpoint
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+    data = read_todo_data()
+    for todo in data:
+        if int(todo["id"]) == id:
+            data.remove(todo)
+
+            with open(my_path_file, "w") as the_file:
+                json.dump(data, the_file, indent=4)
+
+            return {
+                "data": f"Todo with id {id} has been removed."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
+
+
+@app.get("/todo/{id}", tags=["todos"])
+async def get_todo(id: int) -> dict:
+    data = read_todo_data()
+    for todo in data:
+        if int(todo["id"]) == id:
+            return todo
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
