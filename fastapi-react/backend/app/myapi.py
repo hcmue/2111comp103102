0c8c1a02fadec3ab2fa520d8234aa19938168187
Hien Luong <hyhien@gmail.com>
2021-10-26 13:39:45 +0700

    DONE

--- fastapi-react/backend/app/myapi.py
+++ fastapi-react/backend/app/myapi.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, UploadFile, File
+from typing import List
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
 from pathlib import Path
@@ -82,10 +83,27 @@ def read_todo_data():
 
 @app.post("/upload")
 def upload_single_file(file: UploadFile = File(...)):
-    my_path_file = os.path.join(folder.parent, file.filename)
+    my_path_file = os.path.join(folder.parent, "data", file.filename)
     with open(my_path_file, "wb") as my_file:
         shutil.copyfileobj(file.file, my_file)
     return {"filename": file.filename}
+
+
+@app.post("/upload-multiple")
+def upload_multiple_file(files: List[UploadFile] = File(...)):
+    try:
+        my_files = []
+        for file in files:
+            my_path_file = os.path.join(folder.parent, "data", file.filename)
+            my_files.append(file.filename)
+            with open(my_path_file, "wb") as my_file:
+                shutil.copyfileobj(file.file, my_file)
+
+        return {"files": my_files}
+    except Exception as ex:
+        print(ex)
+        logging.error(ex)
+        return HTTPException(status_code=400, detail="Error")
 
 
 @app.post("/todo", tags=["todos"])
